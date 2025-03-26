02b92b86869df2b5e79214a91dd3392b8a1db401
Slava <krylov_s10@mail.ru>
2025-03-26 18:39:23 +0700

    add checkdata /2

--- Desktop/wList/utils/index.ts
+++ Desktop/wList/utils/index.ts
@@ -1,3 +1,5 @@
 export { vintage } from "./src/utils/vintage";
 export { formatPrice } from "./src/utils/formatPrice";
 export { debounce } from "./src/utils/debounce";
+export { showVintage } from "./src/utils/showVintage"
+export { checkData } from "./src/utils/checkData"
